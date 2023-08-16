b862eb309044af4921438f9bb53dfc370aeaf597
vick4 <victorisaacblw@gmail.com>
2023-08-16 12:02:44 -0700

    projectdone

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary is not None:
+        if key in a_dictionary:
+            del a_dictionary[key]
+        return a_dictionary
+
+#    return a_dictionary.pop(key, None)
